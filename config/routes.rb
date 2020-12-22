19ab8471e0dc0a3cb04c7a1db540779296753aba
Jakob Persons <jakobep24@gmail.com>
2020-12-22 11:04:08 -0500

    tests not passing yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destory'
+
+  resources :users, only: [:new, :create]
+
+  root 'welcome#home'
+
+  
 end
